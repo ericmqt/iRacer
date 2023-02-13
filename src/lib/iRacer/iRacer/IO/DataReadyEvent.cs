87ffdccf17be2adf7b7399d4bfe1fc16eff674e4
Eric Smith <33182789+ericmqt@users.noreply.github.com>
2023-02-13 11:56:30 -0500

    Add DataReadyEvent static helper class

---
+++ src/lib/iRacer/iRacer/IO/DataReadyEvent.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.Win32.SafeHandles;
+using Windows.Win32;
+
+namespace iRacer.IO;
+
+internal static class DataReadyEvent
+{
+    internal const string EventName = "Local\\IRSDKDataValidEvent";
+
+    internal static SafeWaitHandle CreateSafeWaitHandle()
+    {
+        return PInvoke.CreateEventAsSafeWaitHandle(null, true, false, EventName);
+    }
+
+    internal static bool TryCreateSafeWaitHandle([NotNullWhen(true)] out SafeWaitHandle? waitHandle)
+    {
+        waitHandle = PInvoke.CreateEventAsSafeWaitHandle(null, true, false, EventName);
+
+        if (waitHandle.IsInvalid || waitHandle.IsClosed)
+        {
+            waitHandle = null;
+            return false;
+        }
+
+        return true;
+    }
+}
