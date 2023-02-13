655184e5df159830e97a9ad585f71aca8adfba15
Eric Smith <33182789+ericmqt@users.noreply.github.com>
2023-02-13 11:52:10 -0500

    Add PInvoke source generator via Microsoft.Windows.CsWin32 package, add CreateEventAsSafeWaitHandle helper method

---
+++ src/lib/iRacer/iRacer/Win32/PInvoke.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Win32.SafeHandles;
+using Windows.Win32.Foundation;
+using Windows.Win32.Security;
+
+namespace Windows.Win32;
+internal static partial class PInvoke
+{
+    internal static unsafe SafeWaitHandle CreateEventAsSafeWaitHandle(SECURITY_ATTRIBUTES? lpEventAttributes, BOOL bManualReset, BOOL bInitialState, string lpName)
+    {
+        fixed (char* lpNameLocal = lpName)
+        {
+            SECURITY_ATTRIBUTES lpEventAttributesLocal =
+                lpEventAttributes.HasValue
+                ? lpEventAttributes.Value
+                : default;
+
+            var handle = CreateEvent(
+                lpEventAttributes.HasValue ? &lpEventAttributesLocal : null,
+                bManualReset,
+                bInitialState,
+                lpNameLocal);
+
+            return new SafeWaitHandle(handle, ownsHandle: true);
+        }
+    }
+}
