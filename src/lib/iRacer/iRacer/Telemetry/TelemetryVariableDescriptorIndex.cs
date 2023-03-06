428c4b454bdeb7471c370efbc296756c22a86b6e
Eric Smith <33182789+ericmqt@users.noreply.github.com>
2023-03-06 14:21:23 -0500

    Implement IComparable<T>, IEquatable<T> on TelemetryVariableDescriptorIndex

--- src/lib/iRacer/iRacer/Telemetry/TelemetryVariableDescriptorIndex.cs
+++ src/lib/iRacer/iRacer/Telemetry/TelemetryVariableDescriptorIndex.cs
@@ -1,6 +1,6 @@
 ﻿namespace iRacer.Telemetry;
 
-public readonly struct TelemetryVariableDescriptorIndex
+public readonly struct TelemetryVariableDescriptorIndex : IComparable<TelemetryVariableDescriptorIndex>, IEquatable<TelemetryVariableDescriptorIndex>
 {
     private readonly int _value;
 
@@ -8,9 +8,59 @@ public readonly struct TelemetryVariableDescriptorIndex
     {
         _value = index;
     }
+    public int CompareTo(TelemetryVariableDescriptorIndex other)
+    {
+        return _value.CompareTo(other._value);
+    }
+
+
+    public bool Equals(TelemetryVariableDescriptorIndex other)
+    {
+        return _value == other._value;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is TelemetryVariableDescriptorIndex otherIndex && Equals(otherIndex);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(_value);
+    }
 
     public static implicit operator int(TelemetryVariableDescriptorIndex descriptorIndex)
     {
         return descriptorIndex._value;
+    }
+
+    public static bool operator ==(TelemetryVariableDescriptorIndex left, TelemetryVariableDescriptorIndex right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(TelemetryVariableDescriptorIndex left, TelemetryVariableDescriptorIndex right)
+    {
+        return !(left == right);
+    }
+
+    public static bool operator <(TelemetryVariableDescriptorIndex left, TelemetryVariableDescriptorIndex right)
+    {
+        return left.CompareTo(right) < 0;
+    }
+
+    public static bool operator <=(TelemetryVariableDescriptorIndex left, TelemetryVariableDescriptorIndex right)
+    {
+        return left.CompareTo(right) <= 0;
+    }
+
+    public static bool operator >(TelemetryVariableDescriptorIndex left, TelemetryVariableDescriptorIndex right)
+    {
+        return left.CompareTo(right) > 0;
+    }
+
+    public static bool operator >=(TelemetryVariableDescriptorIndex left, TelemetryVariableDescriptorIndex right)
+    {
+        return left.CompareTo(right) >= 0;
     }
 }
