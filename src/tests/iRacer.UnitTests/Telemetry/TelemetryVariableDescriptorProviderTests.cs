dcc990c76e2a4268048f8887a70b2eb60345b1c5
Eric Smith <33182789+ericmqt@users.noreply.github.com>
2023-03-06 14:16:19 -0500

    Add TelemetryVariableDescriptorProviderTests

---
+++ src/tests/iRacer.UnitTests/Telemetry/TelemetryVariableDescriptorProviderTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iRacer.Telemetry;
+
+namespace iRacer.UnitTests.Telemetry;
+public class TelemetryVariableDescriptorProviderTests
+{
+    [Fact]
+    public void Count_Test()
+    {
+        var builder = new TelemetryVariableDescriptorProviderBuilder();
+
+        var variable1 = builder.Register<int>("Test1");
+        var variable2 = builder.Register<int>("Test2");
+
+        var provider = builder.Build();
+
+        Assert.Equal(2, provider.Count);
+    }
+
+    [Fact]
+    public void Indexer_Test()
+    {
+        var builder = new TelemetryVariableDescriptorProviderBuilder();
+
+        var variable1 = builder.Register<int>("Test1");
+        var variable2 = builder.Register<int>("Test2");
+
+        var provider = builder.Build();
+
+        Assert.Equal(variable1, provider[variable1.DescriptorIndex]);
+        Assert.Equal(variable2, provider[variable2.DescriptorIndex]);
+    }
+}
